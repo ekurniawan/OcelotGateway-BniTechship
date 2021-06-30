1be08cf1ac9a332a8be3cfb24580f8d0fa34249a
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-06-30 14:20:13 +0700

    menambahkan ocelot library

--- OcelotGateway/Program.cs
+++ OcelotGateway/Program.cs
@@ -20,7 +20,9 @@ namespace OcelotGateway
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration(config => config.AddJsonFile($"ocelot.{env}.json"));
                 });
     }
 }
